9d07b9af354725e85f2f62a269786b5e9101d1ab
Almouk <almoukdaadwaar94@outlook.com>
2024-10-24 22:51:33 +0200

    Controller route added

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\Controller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function(){
+    return "Hello, world";
+});
+
+Route::get('/',[Controller::class. 'index']);
